f61d6f52fdd4911bdaeae557dec5e317bbae4ae6
Per-Arne Andersen <per@sysx.no>
2021-04-15 14:05:02 +0200

    added documentation, fixed example

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # hex-ai
 Various AIs for the board game hex, including Monte Carlo Tree Search with the Tsetlin Machine 
+
+## Install
+`pip install git+https://github.com/cair/hex-ai.git`
+## Example
+```python
+from PyHex import Hex11 as Hex
+
+if __name__ == "__main__":
+    hg = Hex.HexGame()
+    winner = -1
+
+    for game in range(10000000):
+        Hex.init(hg)
+
+        player = 0
+
+        while Hex.full_board(hg) == 0:
+            position = Hex.place_piece_randomly(hg, player)
+
+            if Hex.winner(hg, player, position):
+                winner = player
+                break
+
+            player = 1 - player
+
+        if hg.number_of_open_positions >= 75:
+            print("\nPlayer %s wins!\n\n" % (winner, ))
+            Hex.print(hg)
+```
--- tests/example.py
+++ tests/example.py
@@ -1,24 +1,24 @@
-from PyHex import Hex11
+from PyHex import Hex11 as Hex
 
 if __name__ == "__main__":
-    hg = Hex11.HexGame()
+    hg = Hex.HexGame()
     winner = -1
 
     for game in range(10000000):
-        PyHex.init(hg)
+        Hex.init(hg)
 
         player = 0
 
-        while PyHex.full_board(hg) == 0:
-            position = PyHex.place_piece_randomly(hg, player)
+        while Hex.full_board(hg) == 0:
+            position = Hex.place_piece_randomly(hg, player)
 
-            if PyHex.winner(hg, player, position):
+            if Hex.winner(hg, player, position):
                 winner = player
                 break
 
             player = 1 - player
-        print(PyHex.print(hg))
+
         if hg.number_of_open_positions >= 75:
             print("\nPlayer %s wins!\n\n" % (winner, ))
-            PyHex.print(hg)
+            Hex.print(hg)
 
